7adaeeceb09fd64109ce9d0f9d6d7ad56c9fdbdc
Alex Gusev <alex@flancer64.com>
2024-08-02 11:06:37 +0300

    WF-718 Simplify the README for DI

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,23 @@
 /**
  * The factory function to create new app function objects.
  *
- * @param {typeof Sample_Lib_Config} config - The default export as a singleton object.
- * @param {Sample_Lib_Service} service - Use the default export to create a singleton instance.
+ * @param {Sample_Lib_Config} Config - inject as ESM
+ * @param {Sample_Lib_Service} service - inject the default export as a singleton.
  */
 export default function (
     {
-        Sample_Lib_Config: config,
+        Sample_Lib_Config: Config,
         Sample_Lib_Service$: service,
     }
 ) {
-    // modify injected environment (singleton object)
+    // VARS
+    /** @type {{default: Sample_Lib_Service}}} */
+    const {
+        default: config
+    } = Config; // destruct the Module and get the default export
+
+    // MAIN
+    // modify injected environment (config is also singleton object defined on the ES module level)
     config.name = 'Sample App';
 
     // create application
